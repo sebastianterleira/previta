79962f536ce86b24e54d779eba5d74dc569bc926
sebastianterleira <seb.terleira1204@gmail.com>
2025-02-11 23:09:03 -0600

    feat: content

--- src/components/Footer/components/Content/index.jsx
+++ src/components/Footer/components/Content/index.jsx
@@ -4,12 +4,12 @@ import { slideUp, opacity } from "./animation";
 import { useInView, motion } from "framer-motion";
 import { useRef } from "react";
 import { linksAbout, linksSocials } from "./Links";
+import LogoBlank from "../../../../assets/img/Logos/logo-blank.png"
 
 export default function Content() {
   return (
     <div className={styles.content}>
       <Section1 />
-      <Section2 />
     </div>
   );
 }
@@ -28,13 +28,7 @@ const Section2 = () => {
 
   return (
     <div ref={refOpacityAnimation} className={styles.section2}>
-      <motion.h1
-        variants={opacity}
-        animate={isInView ? "open" : "closed"}
-        className={styles.section2Heading}
-      >
-        Previta Care
-      </motion.h1>
+      <img src={LogoBlank.src}/> 
       <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>
         Copyright © 2024 PrevitaCare S.A.C
       </motion.p>
@@ -50,46 +44,49 @@ const Nav = () => {
 
   return (
     <div className={styles.nav}>
-      <div ref={colAboutRef} className={styles.navColumn}>
-        <h3 className={styles.navHeading}>Nosotros</h3>
-        {linksAbout.map((word, index) => {
-          return (
-            <span key={index} className={styles.mask}>
-              <Magnetic>
-                <motion.a
-                  href={word.url}
-                  variants={slideUp}
-                  custom={index}
-                  animate={isInViewColAbout ? "open" : "closed"}
-                  key={index}
-                >
-                  {word.text}
-                </motion.a>
-              </Magnetic>
-            </span>
-          );
-        })}
+      <div className={styles.navColumns}>
+        <div ref={colAboutRef} className={styles.navColumn}>
+          <h3 className={styles.navHeading}>Nosotros</h3>
+          {linksAbout.map((word, index) => {
+            return (
+              <span key={index} className={styles.mask}>
+                <Magnetic>
+                  <motion.a
+                    href={word.url}
+                    variants={slideUp}
+                    custom={index}
+                    animate={isInViewColAbout ? "open" : "closed"}
+                    key={index}
+                  >
+                    {word.text}
+                  </motion.a>
+                </Magnetic>
+              </span>
+            );
+          })}
+        </div>
+        <div ref={colSocialRef} className={styles.navColumn}>
+          <h3 className={styles.navHeading}>Social</h3>
+          {linksSocials.map((word, index) => {
+            return (
+              <span key={index} className={styles.mask}>
+                <Magnetic>
+                  <motion.a
+                    href={word.url}
+                    variants={slideUp}
+                    custom={index}
+                    animate={isInViewColSocial ? "open" : "closed"}
+                    key={index}
+                  >
+                    {word.text}
+                  </motion.a>
+                </Magnetic>
+              </span>
+            );
+          })}
+        </div>
       </div>
-      <div ref={colSocialRef} className={styles.navColumn}>
-        <h3 className={styles.navHeading}>Social</h3>
-        {linksSocials.map((word, index) => {
-          return (
-            <span key={index} className={styles.mask}>
-              <Magnetic>
-                <motion.a
-                  href={word.url}
-                  variants={slideUp}
-                  custom={index}
-                  animate={isInViewColSocial ? "open" : "closed"}
-                  key={index}
-                >
-                  {word.text}
-                </motion.a>
-              </Magnetic>
-            </span>
-          );
-        })}
-      </div>
+      <Section2 />
     </div>
   );
 };
