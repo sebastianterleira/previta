e0fbecd6e3c199a251821b4d79c61a40208f211e
sebastianterleira <seb.terleira1204@gmail.com>
2025-02-12 17:10:39 -0600

    refactor: mejorar estilos y ajustar márgenes en la sección Historia

--- src/components/About/Historia/index.jsx
+++ src/components/About/Historia/index.jsx
@@ -1,9 +1,9 @@
-import styles from "./historia.module.css";
+// import styles from "./historia.module.css";
 import Row1 from "./rows/row1";
 
 export default function Historia() {
   return (
-    <section className={styles.section}>
+    <section>
       <>
         <Row1 />
       </>
--- src/components/About/Historia/rows/row1/index.jsx
+++ src/components/About/Historia/rows/row1/index.jsx
@@ -1,78 +1,108 @@
 import styles from "../../row.module.css";
+import { useLayoutEffect, useRef } from "react";
 import { useInView, motion } from "framer-motion";
-import { useRef } from "react";
 import { slideUp } from "../../animation";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 
 import ImagenTest from "../../../../../assets/img/Body/img_2.jpg";
 
 export default function Row1() {
   const textParraf1 =
     "Desde el principio, PrevitaCare se comprometió a ofrecer productos innovadores que no solo mejoren la calidad de vida, sino que también prevengan enfermedades crónicas y riesgos de salud, como embarazos no deseados y anemia.";
-  const textParraf2 = "Resultados rápidos y precisos";
-  const textParraf3 = "Resultados rápidos y precisos";
+  const textParraf2 =
+    "Desde el principio, PrevitaCare se comprometió a ofrecer productos innovadores que no solo mejoren la calidad de vida, sino que también prevengan enfermedades crónicas y riesgos de salud, como embarazos no deseados y anemia.";
+  const textParraf3 =
+    "Desde el principio, PrevitaCare se comprometió a ofrecer productos innovadores que no solo mejoren la calidad de vida, sino que también prevengan enfermedades crónicas y riesgos de salud, como embarazos no deseados y anemia.";
   const parraf1 = useRef(null);
   const parraf2 = useRef(null);
   const parraf3 = useRef(null);
   const isInView1 = useInView(parraf1);
   const isInView2 = useInView(parraf2);
   const isInView3 = useInView(parraf3);
+  const container = useRef(null);
+  const imageContainer = useRef(null);
+  const secondColumn = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.create({
+      trigger: imageContainer.current,
+      pin: true,
+      start: "top-=10px", // La imagen se pinnea cuando su parte superior llega al top del viewport.
+      // Se desactiva el pin cuando la parte superior del segundo párrafo alcanza el 80% del viewport.
+      endTrigger: secondColumn.current,
+      end: "top center",
+    });
+  }, []);
 
   return (
     <>
       <div className={styles.about}>
-        <div className={styles.aboutDescription}>
+        <div ref={container} className={styles.aboutDescription}>
           <div className={styles.imageContainer}>
-            <img src={ImagenTest.src} alt="project image" />
+            <img
+              ref={imageContainer}
+              src={ImagenTest.src}
+              alt="project image"
+            />
           </div>
           <div className={styles.column}>
-            <p ref={parraf1} className={styles.parraf}>
-              {textParraf1.split(" ").map((word, index) => {
-                return (
-                  <span key={index} className={styles.mask}>
-                    <motion.span
-                      variants={slideUp}
-                      custom={index}
-                      animate={isInView1 ? "open" : "closed"}
-                      key={index}
-                    >
-                      {word}
-                    </motion.span>
-                  </span>
-                );
-              })}
-            </p>
-            <p ref={parraf2} className={styles.parraf}>
-              {textParraf2.split(" ").map((word, index) => {
-                return (
-                  <span key={index} className={styles.mask}>
-                    <motion.span
-                      variants={slideUp}
-                      custom={index}
-                      animate={isInView2 ? "open" : "closed"}
-                      key={index}
-                    >
-                      {word}
-                    </motion.span>
-                  </span>
-                );
-              })}
-            </p>
-            <p ref={parraf2} className={styles.parraf}>
-              {textParraf3.split(" ").map((word, index) => {
-                return (
-                  <span key={index} className={styles.mask}>
-                    <motion.span
-                      variants={slideUp}
-                      custom={index}
-                      animate={isInView3 ? "open" : "closed"}
-                      key={index}
-                    >
-                      {word}
-                    </motion.span>
-                  </span>
-                );
-              })}
-            </p>
+            <div>
+              <p ref={parraf1} className={styles.parraf}>
+                {textParraf1.split(" ").map((word, index) => {
+                  return (
+                    <span key={index} className={styles.mask}>
+                      <motion.span
+                        variants={slideUp}
+                        custom={index}
+                        animate={isInView1 ? "open" : "closed"}
+                        key={index}
+                      >
+                        {word}
+                      </motion.span>
+                    </span>
+                  );
+                })}
+              </p>
+            </div>
+            <div>
+              <p ref={parraf2} className={styles.parraf}>
+                {textParraf2.split(" ").map((word, index) => {
+                  return (
+                    <span key={index} className={styles.mask}>
+                      <motion.span
+                        variants={slideUp}
+                        custom={index}
+                        animate={isInView2 ? "open" : "closed"}
+                        key={index}
+                      >
+                        {word}
+                      </motion.span>
+                    </span>
+                  );
+                })}
+              </p>
+            </div>
+            <div ref={secondColumn}>
+              <p ref={parraf3} className={styles.parraf}>
+                {textParraf3.split(" ").map((word, index) => {
+                  return (
+                    <span key={index} className={styles.mask}>
+                      <motion.span
+                        variants={slideUp}
+                        custom={index}
+                        animate={isInView3 ? "open" : "closed"}
+                        key={index}
+                      >
+                        {word}
+                      </motion.span>
+                    </span>
+                  );
+                })}
+              </p>
+            </div>
           </div>
         </div>
       </div>
