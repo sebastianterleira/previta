f437f728e637f5e8766ee2ab3c9e090c58be8a97
sebastianterleira <seb.terleira1204@gmail.com>
2025-02-12 13:32:17 -0600

    refactor: mejorar estilos y eliminar archivos innecesarios en la sección Descripción

--- src/components/Home/Description/index.jsx
+++ src/components/Home/Description/index.jsx
@@ -6,21 +6,23 @@ import BtnRound from "../../subcomponents/BtnRound";
 
 export default function Description() {
   const phrase =
-    "Líderes en soluciones de diagnóstico. Ofrecemos pruebas médicas confiables para cuidar de tu salud y la de los tuyos.";
+    "Líderes en soluciones de diagnóstico";
+  const titleRef = useRef(null);
+  const isInViewTitle = useInView(titleRef);
   const description = useRef(null);
   const isInView = useInView(description);
 
   return (
-    <div ref={description} className={styles.description}>
+    <div className={styles.description}>
       <div className={styles.body}>
-        <p>
+        <p ref={titleRef}>
           {phrase.split(" ").map((word, index) => {
             return (
               <span key={index} className={styles.mask}>
                 <motion.span
                   variants={slideUp}
                   custom={index}
-                  animate={isInView ? "open" : "closed"}
+                  animate={isInViewTitle ? "open" : "closed"}
                   key={index}
                 >
                   {word}
@@ -29,16 +31,24 @@ export default function Description() {
             );
           })}
         </p>
-        <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>
-          Con un enfoque en la precisión y la rapidez, nuestras pruebas están
-          diseñadas para brindarte la tranquilidad que necesitas
+        <motion.p
+          ref={description}
+          variants={opacity}
+          animate={isInView ? "open" : "closed"}
+        >
+          Ofrecemos pruebas médicas confiables para cuidar de tu salud y la de
+          los tuyos. Con un enfoque en la precisión y la rapidez, nuestras
+          pruebas están diseñadas para brindarte la tranquilidad que necesitas.
         </motion.p>
-        <a href="/nosotros" data-scroll data-scroll-speed={0.1}>
-          <BtnRound className={styles.button}>
-            <p>About me</p>
-          </BtnRound>
-        </a>
       </div>
     </div>
   );
+}
+
+{
+  /* <a href="/nosotros" data-scroll data-scroll-speed={0.1}>
+          <BtnRound className={styles.button}>
+            <p>About me</p>
+          </BtnRound>
+        </a> */
 }
